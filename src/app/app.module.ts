e14ce11819aa55d1943fdc36f0ae41249b21230c
ahmedbadawy13-gh <ahmedbadawy13@hotmail.com>
2026-01-30 15:08:31 -0800

    fix import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import { App } from './app';
 import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
-  declarations: [AppComponent, ChatComponent],
+  declarations: [App, ChatComponent],
   imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [App],
 })
 export class AppModule {}
